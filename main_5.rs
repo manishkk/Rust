9b2197013da78358ea1f2fb63f9c1dfeaea8bf0b
Manish Kumar <manishkk@users.noreply.github.com>
2018-07-17 23:14:33 +0200

    loop

---
+++ main_5.rs
@@ -0,0 +1,14 @@
+fn main_5() {
+   
+    let mut n = 0;
+    while n <= 10 { //while loop
+    	println!("n is {}", n);
+    	n += 1;
+    }
+
+
+    let a = [10, 20, 30, 40, 50]; // For loop
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+}
