a19678ab33359e0b58f011895c6946f19d228bc4
Manish Kumar <manishkk@users.noreply.github.com>
2018-07-17 23:09:30 +0200

    loop keyword

---
+++ main_4.rs
@@ -0,0 +1,20 @@
+fn main_4() {
+   
+    let mut n = 0;
+  
+    loop {
+
+ 	  		n+=1;
+
+ 	  		if n == 7 {
+    			continue;
+    		}
+
+
+    		if n > 10 {
+    			break;
+    		}
+    		println!("{}", n);
+    }
+    
+}
