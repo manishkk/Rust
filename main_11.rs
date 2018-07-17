26e40a6bbf0f9b2b6b5bea6c61074b88aaeb1945
Manish Kumar <manishkk@users.noreply.github.com>
2018-07-17 23:52:29 +0200

    struct in Rust

---
+++ main_11.rs
@@ -0,0 +1,16 @@
+
+struct Color {
+	red: u8 // 0 -2555
+	green: u8
+	blue: u8
+}
+
+fn main_11(){
+
+	//color: red, green, blue
+
+let bg = Color{red: 255, green: 70, blue: 15};
+
+println!("{},{},{}",bg.red,bg.green, bg.blue);
+
+}
