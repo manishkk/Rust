08fe3354dec93cd2954f8b1b602c45af54bd0526
Manish Kumar <manishkk@users.noreply.github.com>
2018-07-17 23:05:50 +0200

    if-else condition

---
+++ mani_3.rs
@@ -0,0 +1,16 @@
+fn main_3() {
+    let number = 130;
+
+    //is-else condition
+    if number < 50 {
+        println!("Number is less then 50");
+    } else {
+        println!("Number is not less then 50");
+    }
+
+    let a = 3;
+    if a != 0 {
+        println!("number is three");
+       }
+
+}
