2c8568cfacc370273c0f6ecb1ac7b3f2bea1fd67
Manish Kumar <manishkk@users.noreply.github.com>
2018-07-17 23:40:44 +0200

    Shadowing

---
+++ main_9.rs
@@ -0,0 +1,15 @@
+fn main_9() {
+		//shadowing is kind of reassning the variables
+		let mut x = 10;
+
+		{
+			let x = 15;
+			
+		}
+
+	let x = "x is a string";
+	println!("x is {}", x);
+
+	let x = true;
+	println!("x is {}", x);
+	}
