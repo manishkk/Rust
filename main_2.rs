95bd320c3aef5411051aa97a3e475b72c4f86654
Manish Kumar <manishkk@users.noreply.github.com>
2018-07-17 22:38:26 +0200

    Different types of data types

---
+++ main_2.rs
@@ -0,0 +1,13 @@
+fn main_2() {
+   
+  let x = 60; //i32
+  let y: i64 = 70; //i64
+  let z: u64 = 80; //u64
+  let b: bool = false; //Boolean values
+  println!("The value of x is: {}", x);
+  println!("The value of y is: {}", y);
+  println!("The value of z is: {}", z);
+  println!("The value of b is: {}", b);
+  
+  // Here discussed about different types of data types
+}
